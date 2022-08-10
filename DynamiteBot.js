98c29d4b746cf0a7ece48f93d1417a1c501c764f
Oli Goodall <Oli.Goodall@softwire.com>
2022-08-10 12:12:10 +0100

    DynamiteBot Ver 2

--- DynamiteBot.js
+++ DynamiteBot.js
@@ -1,13 +1,13 @@
 class Bot {
-    
+    Dcount = 0   
     makeMove(gamestate) {
-
-        if(this.Dcount <= 100){
+        
+        if(this.Dcount < 100){
             
             if(gamestate.rounds.length === 0){
                 this.score = 0;
                 this.score+=1;
-                this.Dcount = 0;
+                this.Dcount += 1;
                 return 'D';
             }
             if(gamestate.rounds[0].p2 !== 'W'){ // Dynamite can blow everything up except waterbomb
@@ -17,15 +17,37 @@ class Bot {
             else{
                 return 'S'; // Cut the waterbomb with scissors
             }
-
-//            if(gamestate.rounds[gamestate.rounds.length-1].p1 === 'D'){ //check player 1 last move to update dynamite counter
-//            this.Dcount +=1 ;
-//            console.log(this.Dcount);
             
-        }else{
-            
+        }else if (this.Dcount === 100){
+            this.previousMoves = []
+            for (let i = 0; i < gamestate.rounds.length[100]; i++){
+               this.previousMoves.push(gamestate.rounds[i].p2)
+            }
+            function mostCommon(arr){
+                return arr.sort((a,b) =>
+                      arr.filter(v => v===a).length
+                    - arr.filter(v => v===b).length
+                ).pop();
+            }
+            if (mostCommon(this.previousMoves) === "S"){
+            return "R"
+        } if (mostCommon(this.previousMoves) === "P"){
+            return "S"
+        } if (mostCommon(this.previousMoves) === "R"){
+            return "P"
+        } else {
+            return "W"
         }
+
+        // }else if (gamestate.rounds.length >= 101){
+            
+        // }
         
     }
+    else {
+        return "W" 
+    }
 }
-module.exports = new Bot();
+}
+
+module.exports = new Bot ();
